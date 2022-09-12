403dd4f02fbf7496046eeec8677f12534b544898
Theo-Ba <snibot123@gmail.com>
2022-09-12 08:55:49 -0700

    put Log in onCreate because I forgot

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
         setContentView(R.layout.activity_main)
     }
 
