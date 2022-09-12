407b85c9ed96f84ac13be6c57f83774988d1d462
Theo-Ba <snibot123@gmail.com>
2022-09-12 08:52:11 -0700

    put Logs in each lifecycle method

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -22,4 +22,23 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onStart: ")
     }
 
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
 }
