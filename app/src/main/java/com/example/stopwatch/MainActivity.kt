26cee82e55f4437881d23b7e04302af714aa096d
Theo-Ba <snibot123@gmail.com>
2022-09-12 09:45:28 -0700

    toggle start stop works

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
+import android.widget.Button
+import android.widget.Chronometer
+import kotlinx.coroutines.NonCancellable.start
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,10 +16,46 @@ class MainActivity : AppCompatActivity() {
         val TAG = "MainActivity"
     }
 
+    private lateinit var startButton: Button
+    private lateinit var resetButton: Button
+    private lateinit var timer: Chronometer
+    private var stopped = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate")
         setContentView(R.layout.activity_main)
+        initializeWidgets()
+        startButton.setOnClickListener {
+            startTimer()
+        }
+
+        resetButton.setOnClickListener {
+            resetTimer()
+        }
+    }
+
+    private fun resetTimer() {
+
+    }
+
+    private fun startTimer() {
+        if(stopped) {
+            timer.start()
+            startButton.text = "STOP"
+            stopped = false
+        }
+        else {
+            startButton.text = "START"
+            timer.stop()
+            stopped = true
+        }
+    }
+
+    private fun initializeWidgets() {
+        startButton = findViewById(R.id.button_main_start)
+        resetButton = findViewById(R.id.button_main_reset)
+        timer = findViewById(R.id.chronometer_main_stopwatch)
     }
 
     override fun onStart() {
