0cf5df412977093a1bae18f796389b8429543d22
Theo-Ba <snibot123@gmail.com>
2022-09-14 10:59:01 -0700

    implementing a settings

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -8,7 +8,11 @@ import android.os.SystemClock
 import android.util.Log
 import android.widget.Button
 import android.widget.Chronometer
+import android.widget.Switch
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.Group
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import kotlinx.coroutines.NonCancellable.start
 
 class MainActivity : AppCompatActivity() {
@@ -23,8 +27,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var resetButton: Button
     private lateinit var timer: Chronometer
     private lateinit var background: ConstraintLayout
+    private lateinit var settingsButton: Button
+    private lateinit var timerGroup: Group
+    private lateinit var settingsGroup: Group
+    private lateinit var darkMode: Switch
     private var stopped = true
     private var currentTime = 0L
+    private var showingTimer = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +49,34 @@ class MainActivity : AppCompatActivity() {
         resetButton.setOnClickListener {
             resetTimer()
         }
+
+        settingsButton.setOnClickListener {
+            settings()
+        }
+
+        darkMode.setOnCheckedChangeListener { compoundButton, b ->
+            if(b) {
+                startButton.setBackgroundColor(Color.GREEN)
+                startButton.setTextColor(Color.BLACK)
+                resetButton.setBackgroundColor(Color.rgb(115, 11, 0))
+                resetButton.setTextColor(Color.BLACK)
+                background.setBackgroundColor(Color.BLACK)
+                timer.setTextColor(Color.WHITE)
+            }
+        }
+    }
+
+    private fun settings() {
+        if(showingTimer) {
+            timerGroup.isGone = true
+            settingsGroup.isGone = false
+            showingTimer = false
+        }
+        else {
+            timerGroup.isGone = false
+            settingsGroup.isGone = true
+            showingTimer = true
+        }
     }
 
     private fun customizeWidgets() {
@@ -50,6 +87,9 @@ class MainActivity : AppCompatActivity() {
         resetButton.setTextColor(Color.BLACK)
         background.setBackgroundColor(Color.BLACK)
         timer.setTextColor(Color.WHITE)
+        settingsButton.text = ""
+        settingsButton.setBackgroundColor(Color.GRAY)
+        darkMode.toggle()
     }
 
     private fun resetTimer() {
@@ -78,6 +118,10 @@ class MainActivity : AppCompatActivity() {
         resetButton = findViewById(R.id.button_main_reset)
         timer = findViewById(R.id.chronometer_main_stopwatch)
         background = findViewById(R.id.constraintLayout_main_background)
+        settingsButton = findViewById(R.id.button_main_settings)
+        timerGroup = findViewById(R.id.group_main_showTimer)
+        settingsGroup = findViewById(R.id.group_main_settings)
+        darkMode = findViewById(R.id.switch_main_darkMode)
     }
 
     override fun onStart() {
